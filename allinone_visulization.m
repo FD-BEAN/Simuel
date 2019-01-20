b420dac81ac0019a35188292faa60b148e297cd6
FD-BEAN <jing_bin1996@hotmail.com>
2019-01-20 17:15:07 +0800

    Add files via upload

--- allinone_visulization.m
+++ allinone_visulization.m
@@ -2,8 +2,10 @@
 %%
 load for_visualize.mat;
 %%
-figure('Position',[30 150 600 400])
-yLabel = datestr(7.370612e+05:0.000002e+05:7.370632e+05);
+figure('Name','Barge-Vessel Arrangements','NumberTitle','off','Position',[650 150 600 400])
+xlabel('Vessel Number')
+ylabel('Datetime')
+yLabel = datestr(7.370612e+05:0.000002e+05:7.370632e+05,31);
 set(gca,'YTickLabel',yLabel);
 set(gca,'xtick',[1 2 3 4 5 6 7 8 9 10]);
 axis([0,11,737061.2,737063.2]);
@@ -11,12 +13,14 @@ for i = 1:10
     if vis(i,6) == 4
         text(i-0.6,(vis(i,2)+vis(i,3))/2,'Dummy')
         plot([i,i],[vis(i,2),vis(i,3)],'LineWidth',4,'color','b')
+        text(i-0.1,vis(i,3)-0.05,num2str(vis(i,5)),'color','b')
     else
         rectangle('Position',[i-0.25,vis(i,1),0.5,vis(i,4)-vis(i,1)],'EdgeColor','k','LineWidth',2)
         text(i-0.1,(vis(i,4)+vis(i,1))/2,num2str(vis(i,6)))
         hold on
         plot([i,i],[vis(i,1),vis(i,3)],'LineWidth',4,'color','b')
         plot([i,i],[vis(i,2),vis(i,4)],'LineWidth',4,'color','b')
+        text(i-0.1,vis(i,3)-0.05,num2str(vis(i,5)),'color','b')
     end
 end
 
@@ -27,15 +31,22 @@ for j = 1:10
 end
 
 
-figure('Position',[650 150 600 400])
-candle([zeros(4,4);2 4 1 3]);
-rectangle('Position',[4.75,1.5,0.5,0.3],'FaceColor','r','LineWidth',2)
+figure('Name','Legend','NumberTitle','off','Position',[30 150 600 400])
+rectangle('Position',[4.75,2,0.5,1],'LineWidth',2)
+rectangle('Position',[4.75,1.5,0.5,0.5],'FaceColor','r','LineWidth',2)
+hold on
+plot([5,5],[3,4],'LineWidth',4,'color','b')
+plot([5,5],[1,1.5],'LineWidth',4,'color','b')
 xlim([0 10]);
 ylim([0 5]);
 set(gca,'ytick',[1 2 3 4 5]);
 set(gca,'YTickLabel',yLabel);
-text(1.5,4, 'vessel depart time');
-text(1.5,1, 'vessel arrival time');
-text(1.5,2, 'start transfer time');
-text(1.5,3, 'end transfer time');
-text(4.5,2.5,'Barge number');
+text(1.8,4, 'vessel depart time');
+text(1.8,1, 'vessel arrival time');
+text(1.8,2, 'start transfer time');
+text(1.8,3, 'end transfer time');
+text(4.9,2.5,'1');
+text(5.5,2.5,'Barge number');
+text(5.5,1.75,'Terminal Topup');
+text(4.9,0.85,'1','color','b');
+text(5.5,0.85,'Bunker Type')
